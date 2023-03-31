8e7506e3db83ac281e87e6d979c6f4df69c7e935
bzeb33 <93048637+bzeb33@users.noreply.github.com>
2023-03-31 17:00:46 -0400

    Started some application logic to return movie information.  Next is search bar working with the logic for returns.

--- app_server/controllers/home.js
+++ app_server/controllers/home.js
@@ -15,9 +15,38 @@ const results = (req, res) => {
     res.render('search-results', { title: 'Results' });
 };
 
+const fetch = require('node-fetch');
+
+const url = 'https://online-movie-database.p.rapidapi.com/auto-complete?q=Jack';
+
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'bfe68ef249msh8035254c1f65bd7p1e561cjsn36560474497d',
+    'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
+  }
+};
+
+fetch(url, options)
+	.then(res => res.json())
+	.then(json => {
+        const list = json.d;
+
+        list.map((item) => {
+            const name = item.l;
+            const poster = item.i.imageUrl;
+            const movie = `<li><img src="${poster}"> <h2>${name}</h2></li>`
+            document.querySelector('.movies').innerHTML += movie;
+        })
+    })
+	.catch(err => console.error('error:' + err));
+
 
 
 module.exports = {
     homepage,
-    results
+    results,
+    fetch,
+    url,
+    options
 };
