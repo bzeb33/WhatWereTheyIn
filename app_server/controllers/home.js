85c98a52ca2bd79ec656b5f7e6bd201d4b760c21
bzeb33 <93048637+bzeb33@users.noreply.github.com>
2023-04-03 19:24:42 -0400

    Still trying to implement RapidAPI movie API with search bar + logic.

---
+++ app_server/controllers/home.js
@@ -0,0 +1,23 @@
+/* GET homepage */
+const homepage = (req, res) => {
+    res.render('wwti-homepage', { 
+        title: 'What Were They In?',
+        pageHeader: {
+            title: 'What Were They In?',
+            strapline: 'Seriously, what was it?'
+        }
+    });
+};
+
+/* GET results page */
+const results = (req, res) => {
+    //- Set to WWTI-homepage temporarily
+    res.render('search-results', { title: 'Results' });
+};
+
+
+
+module.exports = {
+    homepage,
+    results
+};
