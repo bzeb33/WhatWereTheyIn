4a9a2ad0116c57fcae2429d03e9aa6e8705bbba6
bzeb33 <93048637+bzeb33@users.noreply.github.com>
2023-04-06 18:41:28 -0400

    Beginning schema setup in database js file.  Can't proceed until node is properly upgraded.

--- app_server/models/movies.js
+++ app_server/models/movies.js
@@ -1,1 +1,6 @@
 const mongoose = require('mongoose');
+
+// note for the below: This may not be needed.
+// currently can't test mongodb import info with older node version
+// once upgrade is complete, try import and schema with mongo dashboard FIRST
+const moviesSchema = new mongoose.Schema({ });
