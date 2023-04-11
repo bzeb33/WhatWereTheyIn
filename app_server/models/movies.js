97ad029882cd4dd2e0783d4167dcf70a10598b49
bzeb33 <93048637+bzeb33@users.noreply.github.com>
2023-04-11 17:30:14 -0400

    Began work on movie schema in Mongoose.

--- app_server/models/movies.js
+++ app_server/models/movies.js
@@ -3,4 +3,28 @@ const mongoose = require('mongoose');
 // note for the below: This may not be needed.
 // currently can't test mongodb import info with older node version
 // once upgrade is complete, try import and schema with mongo dashboard FIRST
-const moviesSchema = new mongoose.Schema({ });
+const moviesSchema = new mongoose.Schema({
+    plot: String,
+    genres: [String],
+    runtime: Number,
+    cast: [String],
+    poster: String,
+    num_mflix_comments: Number,
+    title: String,
+    fullplot: String,
+    countries: [String],
+    released: Date,
+    directors: [String],
+    rated: String,
+    awards: String,
+    nominations: String,
+    lastupdated: String,
+    year: Number,
+    imdb: String,
+    votes: Number,
+    type: String,
+    tomatoes: String,
+    numReviews: Number,
+    meter: Number
+
+});
